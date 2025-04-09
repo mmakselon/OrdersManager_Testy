6aedb70cb4cf416fcee76b988f5149686752a0d5
mmakselon <magdalena.pachacz@gmail.com>
2025-04-09 22:05:46 +0200

    add tests in OrderControllerTests

--- OrderManager.UnitTests/Controllers/OrderControllerTests.cs
+++ OrderManager.UnitTests/Controllers/OrderControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,40 @@ namespace OrderManager.UnitTests.Controllers
 {
     internal class OrderControllerTests
     {
+        [Test]
+        public void AddProduct_WhenProductIsNull_ShouldReturnBadRequest()
+        {
+
+        }
+
+        [Test]
+        public void AddProduct_WhenOrderIsLowerThan1_ShouldReturnBadRequest()
+        {
+
+        }
+
+        [Test]
+        public void AddProduct_WhenCalled_ShouldAddProduct()
+        {
+
+        }
+
+        [Test]
+        public void AddProduct_WhenCalled_ShouldReturnOkReasult()
+        {
+
+        }
+
+        [Test]
+        public void AddProduct_WhenOrderServiceFails_ShouldLogError()
+        {
+
+        }
+
+        [Test]
+        public void AddProduct_WhenOrderServiceFails_ShouldReturnBadRequest()
+        {
+
+        }
     }
 }
