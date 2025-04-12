f0bc24b573011a329c605f2ad2e58488753926b7
mmakselon <magdalena.pachacz@gmail.com>
2025-04-12 22:04:02 +0200

    add Init void

--- OrderManager.UnitTests/Controllers/OrderControllerTests.cs
+++ OrderManager.UnitTests/Controllers/OrderControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,42 +11,49 @@ namespace OrderManager.UnitTests.Controllers
 {
     internal class OrderControllerTests
     {
+        private Mock<ILogger> _logger;
+
+        private void Init()
+        {
+
+        }
+
         [Test]
         public void AddProduct_WhenProductIsNull_ShouldReturnBadRequest()
         {
-
+            Init();
 
         }
 
         [Test]
         public void AddProduct_WhenOrderIsLowerThan1_ShouldReturnBadRequest()
         {
-
+            Init();
 
         }
 
         [Test]
         public void AddProduct_WhenCalled_ShouldAddProduct()
         {
-
+            Init();
         }
 
         [Test]
         public void AddProduct_WhenCalled_ShouldReturnOkReasult()
         {
-
+            Init();
         }
 
         [Test]
         public void AddProduct_WhenOrderServiceFails_ShouldLogError()
         {
-
+            Init();
         }
 
         [Test]
         public void AddProduct_WhenOrderServiceFails_ShouldReturnBadRequest()
         {
-
+            Init();
         }
     }
 }
