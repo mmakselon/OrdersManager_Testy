b5d852ce2ba5c566e4a4ef1e444f44cf003b4289
mmakselon <magdalena.pachacz@gmail.com>
2025-04-12 22:10:49 +0200

    update Init method

--- OrderManager.UnitTests/Controllers/OrderControllerTests.cs
+++ OrderManager.UnitTests/Controllers/OrderControllerTests.cs
@@ -2,6 +2,8 @@
 using Moq;
 using NLog;
 using NUnit.Framework;
+using OrdersManager.Controllers;
+using OrdersManager.Core.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +14,15 @@ namespace OrderManager.UnitTests.Controllers
 {
     internal class OrderControllerTests
     {
-        private Mock<ILogger> _logger;
+        private Mock<ILogger> _mockLogger;
+        private Mock<IOrderService> _mockOrderService;
+        private OrderController _orderController;
 
         private void Init()
         {
-
+            _mockLogger = new Mock<ILogger>();
+            _mockOrderService = new Mock<IOrderService>();
+            _orderController = new OrderController(_mockOrderService.Object, _mockLogger.Object);
         }
 
         [Test]
